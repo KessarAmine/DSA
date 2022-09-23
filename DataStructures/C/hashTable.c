cca6cf14a2734a49a056db5b9d304926997250f1
KessarAmine <68372004+KessarAmine@users.noreply.github.com>
2022-09-23 22:04:26 +0200

    h

--- DataStructures/C/hashTable.c
+++ DataStructures/C/hashTable.c
@@ -9,8 +9,8 @@
 
 struct set
 {
-  int key;
-  int data;
+    int key;
+    int data;
 };
 struct set *array;
 int capacity = 10;
@@ -18,101 +18,83 @@ int size = 0;
 
 int hashFunction(int key)
 {
-  return (key % capacity);
+    return (key % capacity);
 }
 int checkPrime(int n)
 {
-  int i;
-  if (n == 1 || n == 0)
-  {
-  return 0;
-  }
-  for (i = 2; i < n / 2; i++)
-  {
-  if (n % i == 0)
-  {
-    return 0;
-  }
-  }
-  return 1;
+    int i;
+    if (n == 1 || n == 0)
+        return (0);
+    for (i = 2; i < n / 2; i++)
+    {
+        if (n % i == 0)
+            return (0);
+    }
+    return (1);
 }
 int getPrime(int n)
 {
-  if (n % 2 == 0)
-  {
-  n++;
-  }
-  while (!checkPrime(n))
-  {
-  n += 2;
-  }
-  return n;
+    if (n % 2 == 0)
+        n++;
+    while (!checkPrime(n))
+        n += 2;
+    return (n);
 }
 void init_array()
 {
-  capacity = getPrime(capacity);
-  array = (struct set *)malloc(capacity * sizeof(struct set));
-  for (int i = 0; i < capacity; i++)
-  {
-  array[i].key = 0;
-  array[i].data = 0;
-  }
+    capacity = getPrime(capacity);
+    array = (struct set *)malloc(capacity * sizeof(struct set));
+    for (int i = 0; i < capacity; i++)
+    {
+        array[i].key = 0;
+        array[i].data = 0;
+    }
 }
 
 void insert(int key, int data)
 {
-  int index = hashFunction(key);
-  if (array[index].data == 0)
-  {
-  array[index].key = key;
-  array[index].data = data;
-  size++;
-  printf("\n Key (%d) has been inserted \n", key);
-  }
-  else if (array[index].key == key)
-  {
-  array[index].data = data;
-  }
-  else
-  {
-  printf("\n Collision occured  \n");
-  }
+    int index = hashFunction(key);
+    if (array[index].data == 0)
+    {
+        array[index].key = key;
+        array[index].data = data;
+        size++;
+        printf("\n Key (%d) has been inserted \n", key);
+    }
+    else if (array[index].key == key)
+        array[index].data = data;
+    else
+        printf("\n Collision occured  \n");
 }
 
 void remove_element(int key)
 {
-  int index = hashFunction(key);
-  if (array[index].data == 0)
-  {
-  printf("\n This key does not exist \n");
-  }
-  else
-  {
-  array[index].key = 0;
-  array[index].data = 0;
-  size--;
-  printf("\n Key (%d) has been removed \n", key);
-  }
+    int index = hashFunction(key);
+    if (array[index].data == 0)
+        printf("\n This key does not exist \n");
+    else
+    {
+        array[index].key = 0;
+        array[index].data = 0;
+        size--;
+        printf("\n Key (%d) has been removed \n", key);
+    }
 }
 void display()
 {
-  int i;
-  for (i = 0; i < capacity; i++)
-  {
-  if (array[i].data == 0)
-  {
-    printf("\n array[%d]: / ", i);
-  }
-  else
-  {
-    printf("\n key: %d array[%d]: %d \t", array[i].key, i, array[i].data);
-  }
-  }
+    int i;
+    for (i = 0; i < capacity; i++)
+    {
+        if (array[i].data == 0)
+            printf("\n array[%d]: / ", i);
+        else
+            printf("\n key: %d array[%d]: %d \t", array[i].key, i, array[i].data);
+    }
 }
 
 int size_of_hashtable()
 {
-  return size;
+    return (size);
 }
 void add1(char *c)
 {
